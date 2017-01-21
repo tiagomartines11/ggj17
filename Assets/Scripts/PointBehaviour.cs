48771b26434a9df1afaf152fa838705e2cb527f4
Tiago Martines <titimartines@gmail.com>
2017-01-21 16:43:19 -0200

    Add ammo indicator.

--- Assets/Scripts/PointBehaviour.cs
+++ Assets/Scripts/PointBehaviour.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private int _Ammo = 3;
+    [SerializeField]
+    private int _Range = 3;
 
-    public int Ammo = 3;
-    public int Projectiles = 1;
-    public float Range = 3;
+    public int Ammo
+    {
+        get { return _Ammo; }
+        set
+        {
+            _Ammo = value;
+            ammoLabel.text = Ammo.ToString();
+        }
+    }
 
+    public int Range
+    {
+        get { return _Range; }
+        set { _Range = value; }
+    }
+
+    private Text ammoLabel;
+    
     // Use this for initialization
     void Start()
     {
+        ammoLabel = GetComponentInChildren<Text>();
+        Ammo = _Ammo;
     }
 
     // Update is called once per frame
