671bc9a42d70da25bc73b853da0dfa4cf53b43e3
Tiago Martines <titimartines@gmail.com>
2017-01-21 15:46:42 -0200

    Add range behaviour.

---
+++ Assets/Scripts/PointBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointBehaviour : MonoBehaviour
+{
+
+    public int Ammo = 3;
+    public int Projectiles = 1;
+    public float Range = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/RangeBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PointBehaviour))]
+public class RangeBehaviour : MonoBehaviour {
+
+    private PointBehaviour pointBehaviour;
+    private GameObject rangeSprite;
+
+	// Use this for initialization
+	void Start () {
+        pointBehaviour = gameObject.GetComponent<PointBehaviour>();
+        rangeSprite = gameObject.transform.Find("Range").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        rangeSprite.transform.localScale = new Vector3(pointBehaviour.Range * 2, pointBehaviour.Range * 2, 0);
+    }
+
+    void OnMouseExit()
+    {
+        rangeSprite.transform.localScale = new Vector3(0, 0, 0);
+    }
+}
