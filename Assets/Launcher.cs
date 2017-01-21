941538dcf2f7ea369b6adeff0f5068d037bc8f61
Epaminondas Neto <epaminondas.neto@tapps.com.br>
2017-01-21 16:57:21 -0200

    projectile

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Launcher : MonoBehaviour {
 
 	public int numProjectiles = 3;
+	public float range = 0.5f;
 	int activeAngle = 0;
+
+	ArrayList angleList = [];
+
+	GameObject aimContainer;
+
 	// Use this for initialization
 	void Start () {
 		activeAngle = 0;
 
 		int cumulativeAngle = 0;
+		aimContainer = new GameObject ();
+
 		for (int i = 0; i < numProjectiles; i++) {
 			GameObject container = new GameObject ();
 
 			GameObject instance = Instantiate (Resources.Load ("aimprefab", typeof(GameObject))) as GameObject;
 
 			instance.transform.parent = container.transform;
-			container.transform.parent = this.transform;
+			container.transform.parent = aimContainer.transform;
 
 			container.transform.localPosition = new Vector3 (0, 0, 0);
 
@@ -30,8 +38,16 @@ public class Launcher : MonoBehaviour {
 
 			instance.transform.localPosition = new Vector3 (1.0f, 0, 0);
 		}
+
+		aimContainer.transform.parent = this.transform;
+		aimContainer.transform.localPosition = new Vector3 (0, 0, 0);
+
 	}
-	
+
+	void OnMouseDown(){
+		launch ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 
@@ -42,13 +58,29 @@ public class Launcher : MonoBehaviour {
 		
 		Quaternion rot = new Quaternion ();
 		rot.eulerAngles = new Vector3 (0, 0, activeAngle);
-		this.transform.rotation = rot;
+		aimContainer.transform.rotation = rot;
 
 
 	}
 
 	void launch()
 	{
+//		Collider2D [] colliders = Physics2D.OverlapCircleAll(this.transform.position, 1f);
+//		if(colliders.Length > 0)
+//		{
+//			// enemies within 1m of the player
+//			Debug.Log(colliders.Length);
+//
+//			Mathf.Sin(
+//		};
+
+
+		for (int i = 0; i < numProjectiles; i++) {
+			GameObject projectile = Instantiate (Resources.Load ("Projectile", typeof(GameObject))) as GameObject;
+			projectile.GetComponent<Projectile> ().SetupProjectile (range, activeAngle);
+			projectile.transform.parent = this.transform;
+
+		}
 
 	}
 }
---
+++ Assets/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	float pRange;
+	float pAngle;
+
+	float pSpeed = 0.1f;
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	public void SetupProjectile(float range, float angle)
+	{
+		this.pAngle = angle;
+		this.pRange = range;
+
+		Quaternion rot = new Quaternion ();
+		rot.eulerAngles = new Vector3 (0, 0, pAngle);
+		this.transform.rotation = rot;
+
+		dir = new Vector3 (Mathf.Cos (angle / 180.0f * Mathf.PI), Mathf.Sin (angle / 180.0f * Mathf.PI), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = this.transform.position + pSpeed * dir;
+	}
+}
