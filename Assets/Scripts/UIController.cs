7673da4608c43fb16c957eea3efb4b7960ee86b8
Tiago Martines <titimartines@gmail.com>
2017-01-21 14:02:38 -0200

    Basic project setup.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
