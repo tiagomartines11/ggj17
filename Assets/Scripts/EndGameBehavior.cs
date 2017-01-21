8128f8b5623049e2ec2a38972e0635f78f13119e
Epaminondas Neto <epaminondas.neto@tapps.com.br>
2017-01-21 19:27:03 -0200

    end game behavior

---
+++ Assets/Scripts/EndGameBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void SetupScreen(boolean isWin, int goalScore, int subGoalScore)
+	{
+		gameObject.transform.Find("goalScore").GetComponent<Text>.().Text = goalScore.ToString();
+		gameObject.transform.Find("subGoalScore").GetComponent<Text>.().Text = subGoalScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
