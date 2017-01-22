e518e9543bd72b6bc0cf46970765ec852261b146
Epaminondas Neto <epaminondas.neto@tapps.com.br>
2017-01-22 05:51:44 -0200

    bgm playing

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		AudioSource fxSound = GetComponent<AudioSource> ();
+		fxSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
