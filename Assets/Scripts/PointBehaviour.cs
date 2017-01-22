3b6ae1b49ff3ef7378e02504c1f2c762810bce90
Epaminondas Neto <epaminondas.neto@tapps.com.br>
2017-01-22 01:03:12 -0200

    fixing hp shield error

--- Assets/Scripts/PointBehaviour.cs
+++ Assets/Scripts/PointBehaviour.cs
@@ -61,7 +61,7 @@ public class PointBehaviour : MonoBehaviour
 			Debug.Log ("i have shields!");
 			shields.disableShield ();
 
-			if(shields.activeShields > 0)
+			if(shields.activeShields >= 0)
 				return;
 		}
 
